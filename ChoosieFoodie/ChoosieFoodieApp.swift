3b8760deaa54c37760e28b0be16f778db3ee176e
laurarosa <laura@fabriziorosas-MacBook-Pro.local>
2022-09-05 21:29:38 +1000

    initialcomit

---
+++ ChoosieFoodie/ChoosieFoodieApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChoosieFoodieApp.swift
+//  ChoosieFoodie
+//
+//  Created by laurarosa on 05.09.22.
+//
+
+import SwiftUI
+
+@main
+struct ChoosieFoodieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
